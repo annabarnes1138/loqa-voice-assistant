a7eb4704cf45a3744099ac842e7f9f8580e3f207
Anna Barnes <33356887+annabarnes1138@users.noreply.github.com>
2025-06-07 09:41:20 -0500

    Initial Commit

---
+++ lite/main.cpp
@@ -0,0 +1,9 @@
+// Entry point for Loqa Lite (AtomS3R + Echo Base)
+
+void setup() {
+  // TODO: Initialize mic, wake word model, and Wi-Fi
+}
+
+void loop() {
+  // TODO: Run inference and communicate with Loqa Prime
+}
